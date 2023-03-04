ffc83e28ca90aea7f4696eb0bee80bfeb6135cad
Rifjan Jundila <rjndla@gmail.com>
2023-03-05 00:22:08 +0700

    test

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+{{-- CONTOH HALAMAN  --}}
+<x-app-layout>
+    @section('content')
+        <div class="content-body">
+            <!-- row -->
+            <div class="container-fluid">
+                {{-- TARUK TAMPILAN DISINI --}}
+            </div>
+        </div>
+    @endsection
+</x-app-layout>
